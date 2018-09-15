14d8fb091719a7b8adbe201d17e4642a1175ea8b
Ali Falahi <ali@afalahi.com>
2018-09-15 09:15:03 -0400

    converted to class based module

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 "use strict";
 require('dotenv').config();
-const Auth = require('./operations/Authenticate');
+const config = require('./operations/configure');
 const request = require('request-promise');
 
-function Client (options) {
-    this.init = () =>{new Auth()}
+class Client {
+    constructor(url, ruleName, ruleKey) {
+        this.url = url,
+        this.ruleName = ruleName,
+        this.ruleKey = ruleKey
+    }
+    init(){
+        config(this.url, this.ruleName, this.ruleKey)
+    }
 }
 
-const client = new Client()
-client.init()
+function client(url, ruleName, ruleKey) {
+    return new Client(url, ruleName, ruleKey)
+}
+
+const cli = client()
+cli.init()
+module.exports = client
--- src/operations/Configure.js
+++ src/operations/Configure.js
@@ -4,46 +4,52 @@ require('dotenv').config();
 const uuid = require('uuid/v1');
 const request = require('request-promise');
 const fs = require('fs');
-const _getCreds 
+const _clientAccount = new WeakMap();
 
 class Configure {
     constructor (url, ruleName, ruleKey){
         this.baseUrl = url || process.env.baseURL;
         this.ruleName = ruleName || process.env.RULE_NAME;
         this.ruleKey = ruleKey || process.env.RULE_KEY
+        _clientAccount.set(this, ()=> {
+            let options = {
+                uri:`${this.baseUrl}/api/v1/sdk-client-accounts`,
+                body:{
+                    clientId: uuid(),
+                    description: `Machine : ${process.env.COMPUTERNAME || process.env.HOSTNAME}, OS : ${process.platform} ${process.arch} - NodeJS ${process.version}`,
+                    name: process.env.COMPUTERNAME || process.env.HOSTNAME,
+                    ruleName: this.ruleName,
+                    onboardingKey: this.ruleKey
+                },
+                json:true
+            };
+            return request.post(options) 
+                .then(res => {
+                    return res;
+                })
+                .then(res => {
+                    let creds = {
+                        client_id: `sdk-client-${res.clientId}`,
+                        client_secret: res.clientSecret,
+                        grant_type: 'client_credentials'
+                    }
+                    fs.writeFile("creds.json",JSON.stringify(creds), function(err, file){
+                        if (err) throw err;
+                    });
+                })
+                .catch(err =>{
+                    throw err;
+                })
+        });
     }
-    getCreds() {
-        let options = {
-            uri:`${this.baseUrl}/api/v1/sdk-client-accounts`,
-            body:{
-                clientId: uuid(),
-                description: `Machine : ${process.env.COMPUTERNAME}, OS : ${process.platform} ${process.arch} - NodeJS ${process.version}`,
-                name: process.env.COMPUTERNAME || process.env.HOSTNAME,
-                ruleName: this.ruleName,
-                onboardingKey: this.ruleKey
-            },
-            json:true
-        };
-        return request.post(options) 
-            .then(res => {
-                return res;
-            })
-            .catch(err =>{
-                throw err;
-            })
-    }
-    setCreds(){
-        Promise.resolve(this.getCreds()).then(res => {
-            let creds = {
-                client_id: `sdk-client-${res.clientid}`,
-                client_secret: res.clientSecret,
-                grant_type: 'client_credentials'
-            }
-            fs.writeFile('.\creds.json',JSON.stringify(creds), function(err, file){
-                if (err) throw err;
-            })
-        })
+    init() {
+        _clientAccount.get(this)();
     }
 }
-const config = new Configure()
-config.setCreds()
+
+function config (url, ruleName, ruleKey) {
+   return new Configure(url,ruleName, ruleKey).init()
+}
+
+
+module.exports = config;
