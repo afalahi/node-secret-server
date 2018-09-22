d44b59dcf6023f5ce019e30fed32320fc1037a3b
Ali Falahi <ali@afalahi.com>
2018-09-22 10:18:16 -0400

    style clean up

--- src/operations/Configure.js
+++ src/operations/Configure.js
@@ -9,61 +9,60 @@ const  protect  = new DataProtection('creds.json')
 const _clientAccount = new WeakMap();
 
 class Configure {
-    
-    constructor (options) {
-        _clientAccount.set(this, () => {
-            let opts = {
-                uri:`${options.baseUrl}/api/v1/sdk-client-accounts`,
-                body:{
-                    clientId: uuid(),
-                    description: `Machine : ${process.env.COMPUTERNAME || process.env.HOSTNAME}, OS : ${process.platform} ${process.arch} - NodeJS ${process.version}`,
-                    name: process.env.COMPUTERNAME || process.env.HOSTNAME,
-                    ruleName: options.ruleName,
-                    onboardingKey: options.ruleKey
-                },
-                json:true
-            };
-            return request.post(opts) 
-                .then(res => {
-                    return res;
-                })
-                .then(res => {
-                    let creds = {
-                        client_id: `sdk-client-${res.clientId}`,
-                        client_secret: res.clientSecret,
-                        grant_type: 'client_credentials'
-                    }
-                    // fileSystem.writeFile(`creds.json`,JSON.stringify(creds), function(err, file) {
-                    //     if (err) throw err;
-                    // });
-                    protect.encrypt(creds)
-                        .then(res => {
-                            return res;
-                        })
-                        .catch(err => {
-                            throw err;
-                        });
-                })
-                .catch(err => {
-                    throw err;
-                });
+  constructor (options) {
+    _clientAccount.set(this, () => {
+      let opts = {
+        uri:`${options.baseUrl}/api/v1/sdk-client-accounts`,
+        body:{
+          clientId: uuid(),
+          description: `Machine : ${process.env.COMPUTERNAME || process.env.HOSTNAME}, OS : ${process.platform} ${process.arch} - NodeJS ${process.version}`,
+          name: process.env.COMPUTERNAME || process.env.HOSTNAME,
+          ruleName: options.ruleName,
+          onboardingKey: options.ruleKey
+        },
+        json:true
+      };
+      return request.post(opts) 
+        .then(res => {
+          return res;
+        })
+        .then(res => {
+          let creds = {
+            client_id: `sdk-client-${res.clientId}`,
+            client_secret: res.clientSecret,
+            grant_type: 'client_credentials'
+          }
+          protect.encrypt(creds)
+            .then(res => {
+              return res;
+            })
+            .catch(err => {
+              throw err;
+            });
+        })
+        .catch(err => {
+          throw err;
         });
+    });
+  }
+
+  saveCredentials() {
+    if (fileSystem.existsSync("creds.json")) {
+      throw "Client Already Initialized"
     }
 
-    saveCredentials() {
-        if(fileSystem.existsSync("creds.json")) {
-            throw "Client Already Initialized"
-        }
-        _clientAccount.get(this)();
+    _clientAccount.get(this)();
+  }
+
+  loadCredentials() {
+    if (!(fileSystem.existsSync('creds.json'))) {
+      throw "Client not initialized"
     }
 
-    loadCredentials() {
-        if(!(fileSystem.existsSync('creds.json'))) {
-            throw "Client not initialized"
-        }
-        let creds = protect.decrypt().then(res => {return res}).catch(err => {throw err});
-        return creds
-    }
+    let creds = protect.decrypt().then(res => {return res}).catch(err => {throw err});
+    
+    return creds
+  }
 }
 
 module.exports = Configure;
