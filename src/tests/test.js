360c5669ab1382049190ce0694f17333fd878ddb
Ali Falahi <ali@afalahi.com>
2018-09-24 12:56:25 -0400

    misc

--- src/tests/test.js
+++ src/tests/test.js
@@ -4,44 +4,8 @@ const crypto = require('crypto');
 //const DataProtection = require('../lib/DataProtection');
 //const protect = new DataProtection()
 
-
 const client = new SecretServer()
 //client.init()
-//Promise.resolve(client.accessToken()).then(res => {console.log(res)})
-client.remove()
-
-// let masterKey
-// try{
-//     if(!(fileSystem.existsSync('masterKey.json'))){
-//         masterKey = crypto.randomBytes(32).toString('base64')
-//         fileSystem.writeFileSync('masterKey.json',JSON.stringify({key:masterKey}));
-//     } else {
-//         masterKey = JSON.parse(fileSystem.readFileSync('masterKey.json')).key
-//         console.log(masterKey)
-//     }
-//     let salt = crypto.randomBytes(64);
-//     let key = crypto.pbkdf2Sync(masterKey, salt, 2145, 32, 'sha512');
-//     console.log(key.toString('base64'));
-//     return key;
-// } catch (e) {
-//     throw new Error(e.message);
-// }
-// function getKey(salt, callback){
-//     if(callback === undefined){
-//         return crypto.pbkdf2Sync("thisisapassword", salt, 2145, 32, 'sha512');
-//     }
-//     crypto.pbkdf2("thisisapassword", salt, 2145, 32, 'sha512', (err, derivedKey) =>{
-//         if (err){
-//             return callback(err);
-//         }
-//         callback(null, derivedKey)
-//     });
-// }
-// let key = getKey('thisissalt', function(err, buffer){
-//     if(err) {
-//         throw err;
-//     }
-//     return buffer
-// })
-
-// console.log(key);
+Promise.resolve(client.readSecret({uri:'api/v1/secrets/5'})).then(res => {console.log(res)})
+//client.remove()
+//Promise.resolve(client.accessToken()).then(res => {console.log(JSON.parse(res).access_token)})
