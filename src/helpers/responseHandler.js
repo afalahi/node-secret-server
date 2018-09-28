6bc162b98a197706c30acec1da5bf6376122a183
Ali Falahi <ali@afalahi.com>
2018-09-27 22:35:30 -0400

    generic error and response handling

---
+++ src/helpers/responseHandler.js
@@ -0,0 +1,27 @@
+const ErrorHandler = require('./ErrorHandler');
+const ApiError = ErrorHandler.ErrorHandler;
+const SecretServerError = ErrorHandler.SecretServerError;
+
+class ResponseHandler {
+  constructor(response) {
+    if (!response) {
+      return Promise.reject(new SecretServerError('No response passed'));
+    }
+
+    if (response.statusCode !== 200 && response.statusCode !== 204) {
+      let message;
+
+      if (response.body && response.body.errorCode && response.body.errorCode.length > 0) {
+        message = response.body.errorCode;
+      } else {
+        message = `Status ${response.statusCode}`;
+      }
+
+      const error = new ApiError(message, response);
+      return Promise.reject(error);
+    }
+    return Promise.resolve(response.body);
+  }
+}
+
+module.exports = ResponseHandler
